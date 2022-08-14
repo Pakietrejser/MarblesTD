5d8e04f7cc11455cf588c2d41ff5acc7055007bc
Bartosz Ptak <bartosz.ptak98@gmail.com>
2022-08-14 14:13:36 +0200

    zenject added

---
+++ Assets/MarblesTD/UnityCore/Installers/GameInstaller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace MarblesTD.UnityCore.Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            // Container.BindMemoryPool<>()
+        }
+    }
+
+    public class TestMono : MonoBehaviour
+    {
+        public void Populate(int test)
+        {
+            
+        }
+
+        public void Clear()
+        {
+            
+        }
+    }
+    
+    // public class Pool : MonoMemoryPool<>
+}
