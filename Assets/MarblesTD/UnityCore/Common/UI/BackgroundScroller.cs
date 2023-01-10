041a68993290dc77cae734e742b9591e503e6ef6
Bartosz Ptak <bartosz.ptak98@gmail.com>
2023-01-10 02:32:29 +0100

    glam 1

---
+++ Assets/MarblesTD/UnityCore/Common/UI/BackgroundScroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MarblesTD.UnityCore.Common.UI
+{
+    public class BackgroundScroller : MonoBehaviour
+    {
+        [SerializeField] RawImage image;
+        [SerializeField] float x;
+        [SerializeField] float y;
+
+        void Update()
+        {
+            image.uvRect = new Rect(image.uvRect.position + new Vector2(x, y) * Time.deltaTime, image.uvRect.size);
+        }
+    }
+}
