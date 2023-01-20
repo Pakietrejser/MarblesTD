29209df5c5b1caa3bd29fc80a33fc8119971485c
Bartosz Ptak <bartosz.ptak98@gmail.com>
2023-01-20 17:02:33 +0100

    upgrades

---
+++ Assets/MarblesTD/Towers/StarStagUpgrades.cs
@@ -0,0 +1,60 @@
+﻿using MarblesTD.Core.Entities.Towers;
+
+namespace MarblesTD.Towers
+{
+    public class MorePower : Upgrade<StarStag>
+    {
+        public override int Cost => 200;
+        public override string Description => "Zwiększa szybkość najbliższego sojusznika.";
+        
+        protected override void ExplicitApply(StarStag tower)
+        {
+        }
+    }
+    
+    public class EvenMorePower : Upgrade<StarStag>
+    {
+        public override int Cost => 500;
+        public override string Description => "ZNACZNIE zwiększa szybkość najbliższego sojusznika.";
+        protected override void ExplicitApply(StarStag tower)
+        {
+        }
+    }
+    
+    public class Twins : Upgrade<StarStag>
+    {
+        public override int Cost => 200;
+        public override string Description => "Wspiera do dwóch najbliższych sojuszników.";
+        
+        protected override void ExplicitApply(StarStag tower)
+        {
+        }
+    }
+    
+    public class Eden : Upgrade<StarStag>
+    {
+        public override int Cost => 500;
+        public override string Description => "Wspiera WSZYSTKICH sojuszników.";
+        protected override void ExplicitApply(StarStag tower)
+        {
+        }
+    }
+
+    public class Boom : Upgrade<StarStag>
+    {
+        public override int Cost => 10;
+        public override string Description => "NATYCHMIAST zbija 1 warstę każdej marbli.";
+        protected override void ExplicitApply(StarStag tower)
+        {
+        }
+    }
+    
+    public class Gloom : Upgrade<StarStag>
+    {
+        public override int Cost => 1000;
+        public override string Description => "NATYCHMIAST niszczy wszystkie marble.";
+        protected override void ExplicitApply(StarStag tower)
+        {
+        }
+    }
+}
---
+++ Assets/MarblesTD/Towers/WebWeaverUpgrades.cs
@@ -0,0 +1,60 @@
+﻿using MarblesTD.Core.Entities.Towers;
+
+namespace MarblesTD.Towers
+{
+    public class Bite : Upgrade<WebWeaver>
+    {
+        public override int Cost => 1;
+        public override string Description => "Raz na jakiś czas gryzie wszystkie Marble w sieci.";
+        
+        protected override void ExplicitApply(WebWeaver tower)
+        {
+        }
+    }
+    
+    public class SuperBite : Upgrade<WebWeaver>
+    {
+        public override int Cost => 1;
+        public override string Description => "Ugryzienie zadaje OGROMNE obrażenia.";
+        protected override void ExplicitApply(WebWeaver tower)
+        {
+        }
+    }
+    
+    public class BiggerWeb : Upgrade<WebWeaver>
+    {
+        public override int Cost => 1;
+        public override string Description => "Zwiększa obszar sieci.";
+        
+        protected override void ExplicitApply(WebWeaver tower)
+        {
+        }
+    }
+    
+    public class PoisonousWeb : Upgrade<WebWeaver>
+    {
+        public override int Cost => 1;
+        public override string Description => "Sieć ZATRUWA marble.";
+        protected override void ExplicitApply(WebWeaver tower)
+        {
+        }
+    }
+
+    public class Sticky : Upgrade<WebWeaver>
+    {
+        public override int Cost => 1;
+        public override string Description => "Spowolnienie działa 3 sekundy po wyjściu z sieci.";
+        protected override void ExplicitApply(WebWeaver tower)
+        {
+        }
+    }
+    
+    public class WebWorld : Upgrade<WebWeaver>
+    {
+        public override int Cost => 1;
+        public override string Description => "NATYCHMIAST spowalnia wszystkie marble.";
+        protected override void ExplicitApply(WebWeaver tower)
+        {
+        }
+    }
+}
